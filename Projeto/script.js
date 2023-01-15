fce6bdc47abfb0a6a6444c2624855f046bf1996d
pedrob14 <pedrob14farias@gmail.com>
2023-01-15 12:59:32 -0300

    Atualizando atributos HTML com JavaScript
    Finalizando sessão JavaScript

--- Projeto/script.js
+++ Projeto/script.js
@@ -6,6 +6,25 @@ function toggleMode() {
   } else {
     html.classList.add('light')
   } ou*/
+  html.classList.toggle("light") //Dentro da função toogle há esse conceito de add e remover.~
 
-  html.classList.toggle('light') //Dentro da função toogle há esse conceito de add e remover.
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+  //substituir a imagem e txt
+  if (html.classList.contains("light")) {
+    //se tiver light mode, adicionar a imagem e txt light
+    //Add ou modificar um atributo. Aqui no caso a imagem e txt!
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Pedro Farias sério, usando camisa jeans, barba e fundo borrado."
+    )
+  } else {
+    //se tiver sem light mode, manter a imagem e txt normal
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Pedro Farias sorrindo, usando camisa azul, barba e fundo branco."
+    )
+  }
 }
